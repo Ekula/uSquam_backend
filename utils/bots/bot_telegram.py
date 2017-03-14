4587017988fefd662277e2b91424efd1309b57cc
Jan Harms <jan.gerrit.harms@gmail.com>
2017-03-14 10:47:08 +0100

    Add InteractionManager as proxy for messages

--- utils/bots/bot_telegram.py
+++ utils/bots/bot_telegram.py
@@ -1,15 +1,26 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler
 from utils.secrets import TELEGRAM_KEY
 from resources.task.service import TaskService
+from lib.interaction_manager import InteractionManager
+
+telegramIM = InteractionManager()
 
 def start(bot, update):
-    update.message.reply_text('Hello World')
+    result = telegramIM.onInput(update.message.from_user.id, 'start')
+    print result
+    update.message.reply_text(result)
 
-def hello(bot, update):
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name)
-    )
+def task(bot, update):
+    result = telegramIM.onInput(update.message.from_user.id, 'task')
+    print result
+    update.message.reply_text(result)
+
+def message(bot, update):
+    result = telegramIM.onInput(update.message.from_user.id, update.message.text)
+    print result
+    update.message.reply_text(result)
 
 updater = Updater(TELEGRAM_KEY)
 updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('task', hello))
+updater.dispatcher.add_handler(CommandHandler('task', task))
+updater.dispatcher.add_handler(MessageHandler(None, message))
