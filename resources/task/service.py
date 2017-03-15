e1eff72f118c26494efa92121f7f5757999c0027
Jan Harms <jan.gerrit.harms@gmail.com>
2017-03-15 23:24:47 +0100

    Fully integrated TaskService

--- resources/task/service.py
+++ resources/task/service.py
@@ -1,35 +1,16 @@
 from task_model import Task
 
 class Service:
-    TASKS = [
-        {
-            'questions': [
-                {
-                    'id': 1,
-                    'query': 'Make a selfie with a happy face'
-                }
-            ]
-        }
-    ]
-
     def getAll(self):
-        return self.TASKS
+        return Task.objects
     
     def get(self, id):
-        tasks = self.getAll()
-
-        if id < len(tasks):
-            return tasks[id]
-        else:
-            return None
+        return Task.objects.get(id=id)
     
     def insert(self, in_task):
         task = Task()
         task.name = in_task['name']
         task.save()
-        if not task:
-            return False
-        self.TASKS.append(task)
         return True
 
 TaskService = Service()
