de9805364eda97f9a0cbdc0c867a4db1be26a155
Jan Harms <jan.gerrit.harms@gmail.com>
2017-03-16 13:28:13 +0100

    Fixed the GET request for task

--- resources/register.py
+++ resources/register.py
@@ -5,5 +5,5 @@ from task.blueprint import Task, TaskList
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
-api.add_resource(Task, '/tasks/<int:id>')
+api.add_resource(Task, '/tasks/<string:id>')
 api.add_resource(TaskList, '/tasks')
