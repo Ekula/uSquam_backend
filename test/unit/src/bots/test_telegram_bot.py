133341289f9e7db92eb06db0aa01ef3c7ca00a41
Jan Harms <jan.gerrit.harms@gmail.com>
2017-04-01 23:47:38 +0200

    Added more intent files and wildcard parsing for answers

--- test/unit/src/bots/test_telegram_bot.py
+++ test/unit/src/bots/test_telegram_bot.py
@@ -4,12 +4,12 @@ from src.bots.bot_telegram import start, task, tasks
 
 class TelegramBotTest(unittest.TestCase):
 
-    @mock.patch('usquam.src.bots.bot_telegram.InteractionRedirector')
+    @mock.patch('src.bots.bot_telegram.InteractionRedirector')
     def test_start(self, mock_IR):
-        mock = mock.MagicMock()
-        mock.message.from_user.id = '1'
+        mock_update = mock.MagicMock()
+        mock_update.message.from_user.id = '1'
         mock_IR.onInput.return_value = 'hello'
-        start(None, mock)
+        start(None, mock_update)
 
         self.assertTrue(mock_IR.onInput.called)
-        mock.message.reply_text.assert_called_with('hello')
+        mock_update.message.reply_text.assert_called_with('hello')
